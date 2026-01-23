9db8ae0e5c9eaae5cbe2b347353b90d6bece0917
PaulHalonen <paulhalonen@gmail.com>
2026-01-22 21:04:48 -0600

    "Part10-GradleFix-AppWorking"

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file for TrueVault Helper Android App
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
